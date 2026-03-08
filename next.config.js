8330491011e2b83a99e7e17496aded1641cdfae8
dwagllc-a11y <dwagllc@gmail.com>
2026-03-08 18:12:38 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
